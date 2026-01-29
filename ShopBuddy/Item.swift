d0d232448f4a816dd59ac4c12050638b28ca55d4
Daniil Kozar <daqua.998@gmail.com>
2026-01-29 11:05:14 -0700

    Initial Commit

---
+++ ShopBuddy/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ShopBuddy
+//
+//  Created by Dan on 1/29/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
